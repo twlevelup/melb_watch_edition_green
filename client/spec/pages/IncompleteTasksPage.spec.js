3e437aab4ec9aee549e8c8ae89e70096e8f54bc5
Lynne Nguyen <s3433771@student.rmit.edu.au>
2015-10-17 10:28:06 +1100

    uncomment incomplete test

--- client/spec/pages/IncompleteTasksPage.spec.js
+++ client/spec/pages/IncompleteTasksPage.spec.js
@@ -1,77 +1,77 @@
-// 'use strict';
+'use strict';
 
-// var TaskPage = require('../../src/js/pages/IncompleteTasksPage'),
-  // Router = require('../../src/js/router'),
-  // App = require('../../src/js/app');
+var TaskPage = require('../../src/js/pages/IncompleteTasksPage'),
+  Router = require('../../src/js/router'),
+  App = require('../../src/js/app');
 
-// global.App = App;
+global.App = App;
 
-// describe('The Incomplete Tasks', function() {
-  // var IncompleteTaskPage;
+describe('The Incomplete Tasks', function() {
+  var IncompleteTaskPage;
 
-  // beforeEach(function() {
-    // IncompleteTaskPage = new TaskPage();
-  // });
+  beforeEach(function() {
+    IncompleteTaskPage = new TaskPage();
+  });
 
-  // describe('button event handlers', function() {
+  describe('button event handlers', function() {
 
-    // describe('left', function() {
+    describe('left', function() {
 
-      // it('should take the user to the home page', function() {
-        // spyOn(global.App, 'navigate');
-        // IncompleteTaskPage.setButtonEvents();
-        // IncompleteTaskPage.trigger('left');
-        // expect(global.App.navigate).toHaveBeenCalledWith('');
-      // });
-    // });
+      it('should take the user to the home page', function() {
+        spyOn(global.App, 'navigate');
+        IncompleteTaskPage.setButtonEvents();
+        IncompleteTaskPage.trigger('left');
+        expect(global.App.navigate).toHaveBeenCalledWith('');
+      });
+    });
 
-    // describe('top', function() {
+    describe('top', function() {
 
-      // it('should scroll the watch face up', function() {
-        // spyOn(IncompleteTaskPage, 'scrollUp');
-        // IncompleteTaskPage.setButtonEvents();
-        // IncompleteTaskPage.trigger('top');
-        // expect(IncompleteTaskPage.scrollUp).toHaveBeenCalled();
-      // });
-    // });
+      it('should scroll the watch face up', function() {
+        spyOn(IncompleteTaskPage, 'scrollUp');
+        IncompleteTaskPage.setButtonEvents();
+        IncompleteTaskPage.trigger('top');
+        expect(IncompleteTaskPage.scrollUp).toHaveBeenCalled();
+      });
+    });
 
-    // describe('bottom', function() {
-      // it('should scroll the watch face down', function() {
-        // spyOn(IncompleteTaskPage, 'scrollDown');
-        // IncompleteTaskPage.setButtonEvents();
-        // IncompleteTaskPage.trigger('bottom');
-        // expect(IncompleteTaskPage.scrollDown).toHaveBeenCalled();
-      // });
-    // });
+    describe('bottom', function() {
+      it('should scroll the watch face down', function() {
+        spyOn(IncompleteTaskPage, 'scrollDown');
+        IncompleteTaskPage.setButtonEvents();
+        IncompleteTaskPage.trigger('bottom');
+        expect(IncompleteTaskPage.scrollDown).toHaveBeenCalled();
+      });
+    });
 
-  // });
+  });
 
-  // describe('swap', function() {
-    // it('should swap tasks', function() {
-      // var Tasks = Backbone.Collection.extend({
-        // model: Task,
+  describe('swap', function() {
+    it('should swap tasks', function() {
+      var Tasks = Backbone.Collection.extend({
+        model: Task,
 
-      // });
-      // var Tasks2 = Backbone.Collection.extend({
-        // model: Task,
+      });
+      var Tasks2 = Backbone.Collection.extend({
+        model: Task,
 
-      // });
-      // var floor = {taskNum: '1', taskDescription: 'calibrate the floors'};
-      // IncompleteTaskPage.swap(Tasks, Tasks2,floor);
-    // });
-  // });
+      });
+      var floor = {taskNum: '1', taskDescription: 'calibrate the floors'};
+      IncompleteTaskPage.swap(Tasks, Tasks2,floor);
+    });
+  });
 
-  // describe('rendering', function() {
+  describe('rendering', function() {
 
-    // it('should produce the correct HTML', function() {
-      // IncompleteTaskPage.render();
-      // expect(IncompleteTaskPage.$el).toContainText('Task');
-    // });
+    it('should produce the correct HTML', function() {
+      IncompleteTaskPage.render();
+      expect(IncompleteTaskPage.$el).toContainText('Task');
+    });
 
-    // it('returns the view object', function() {
-      // expect(IncompleteTaskPage.render()).toEqual(IncompleteTaskPage);
-    // });
+    it('returns the view object', function() {
+      expect(IncompleteTaskPage.render()).toEqual(IncompleteTaskPage);
+    });
 
-  // });
+  });
 
-// });
+});
--- client/spec/pages/doneTasksPage.spec.js
+++ client/spec/pages/doneTasksPage.spec.js
@@ -1,71 +1,71 @@
-// 'use strict';
+'use strict';
 
-// var TaskPage = require('../../src/js/pages/DoneTasksPage'),
-  // Router = require('../../src/js/router'),
-  // App = require('../../src/js/app');
+var TaskPage = require('../../src/js/pages/DoneTasksPage'),
+  Router = require('../../src/js/router'),
+  App = require('../../src/js/app');
 
-// global.App = App;
+global.App = App;
 
-// describe('The Done Tasks', function() {
-  // var DoneTaskPage;
+describe('The Done Tasks', function() {
+  var DoneTaskPage;
 
-  // beforeEach(function() {
-    // DoneTaskPage = new TaskPage();
-  // });
+  beforeEach(function() {
+    DoneTaskPage = new TaskPage();
+  });
 
-  // describe('button event handlers', function() {
+  describe('button event handlers', function() {
 
-    // describe('left', function() {
+    describe('left', function() {
 
-      // it('should take the user to the incomplete tasks page', function() {
-        // spyOn(global.App, 'navigate');
-        // DoneTaskPage.setButtonEvents();
-        // DoneTaskPage.trigger('left');
-        // expect(global.App.navigate).toHaveBeenCalledWith('incompleteTasks');
-      // });
-    // });
+      it('should take the user to the incomplete tasks page', function() {
+        spyOn(global.App, 'navigate');
+        DoneTaskPage.setButtonEvents();
+        DoneTaskPage.trigger('left');
+        expect(global.App.navigate).toHaveBeenCalledWith('incompleteTasks');
+      });
+    });
 
-    // describe('right', function() {
+    describe('right', function() {
 
-      // it('should take the user to the home page', function() {
-        // spyOn(global.App, 'navigate');
-        // DoneTaskPage.setButtonEvents();
-        // DoneTaskPage.trigger('right');
-        // expect(global.App.navigate).toHaveBeenCalledWith('');
-      // });
-    // });
+      it('should take the user to the home page', function() {
+        spyOn(global.App, 'navigate');
+        DoneTaskPage.setButtonEvents();
+        DoneTaskPage.trigger('right');
+        expect(global.App.navigate).toHaveBeenCalledWith('');
+      });
+    });
 
-    // describe('top', function() {
-      // it('should scroll the watch face up', function() {
-        // spyOn(DoneTaskPage, 'scrollUp');
-        // DoneTaskPage.setButtonEvents();
-        // DoneTaskPage.trigger('top');
-        // expect(DoneTaskPage.scrollUp).toHaveBeenCalled();
-      // });
-    // });
+    describe('top', function() {
+      it('should scroll the watch face up', function() {
+        spyOn(DoneTaskPage, 'scrollUp');
+        DoneTaskPage.setButtonEvents();
+        DoneTaskPage.trigger('top');
+        expect(DoneTaskPage.scrollUp).toHaveBeenCalled();
+      });
+    });
 
-    // describe('bottom', function() {
-      // it('should scroll the watch face down', function() {
-        // spyOn(DoneTaskPage, 'scrollDown');
-        // DoneTaskPage.setButtonEvents();
-        // DoneTaskPage.trigger('bottom');
-        // expect(DoneTaskPage.scrollDown).toHaveBeenCalled();
-      // });
-    // });
+    describe('bottom', function() {
+      it('should scroll the watch face down', function() {
+        spyOn(DoneTaskPage, 'scrollDown');
+        DoneTaskPage.setButtonEvents();
+        DoneTaskPage.trigger('bottom');
+        expect(DoneTaskPage.scrollDown).toHaveBeenCalled();
+      });
+    });
 
-  // });
+  });
 
-  // describe('rendering', function() {
+  describe('rendering', function() {
 
-    // it('should produce the correct HTML', function() {
-      // DoneTaskPage.render();
-      // expect(DoneTaskPage.$el).toContainText('Task');
-    // });
+    it('should produce the correct HTML', function() {
+      DoneTaskPage.render();
+      expect(DoneTaskPage.$el).toContainText('Task');
+    });
 
-    // it('returns the view object', function() {
-      // expect(DoneTaskPage.render()).toEqual(DoneTaskPage);
-    // });
+    it('returns the view object', function() {
+      expect(DoneTaskPage.render()).toEqual(DoneTaskPage);
+    });
 
-  // });
+  });
 
-// });
+});
